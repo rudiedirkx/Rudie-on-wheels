67ed671cc2b05cdcf6a63f971d84d9c75d7f45fd
Rudie Dirkx <github@hotblocks.nl>
2011-01-20 00:28:01 +0100

    Little bit of row\auth dabbling...

--- vendors/row/auth/Session.php
+++ vendors/row/auth/Session.php
@@ -3,14 +3,39 @@
 namespace row\auth;
 
 use row\core\Object;
+//use row\auth;
 
 class Session extends Object {
+
+	public $users = array();
+
+	public function __construct() {
+		$this->users[] = new SessionUser; // There is ALWAYS at least one 'layer'
+	}
+
+	public function addUser( SessionUser $user ) {
+		$this->users[] = $user;
+	}
+
+	public function logout() {
+		$user = $this->getUser();
+		if ( !$user->anonymous ) {
+			array_pop($this->users);
+			$user->logout();
+			// Change _SESSION here or in auth\SessionUser?
+			return true;
+		}
+		// Can't 'logout' Anonymous user. Must always have that very first layer
+	}
+
+	public function getUser() {
+		$n = count($this->users);
+		return $this->users[$n-1];
+	}
 
 	public function __tostring() {
 		return 'Session';
 	}
-
-	
 
 }
 
--- vendors/row/auth/SessionUser.php
+++ vendors/row/auth/SessionUser.php
@@ -6,12 +6,23 @@ use row\core\Object;
 
 class SessionUser extends Object {
 
-	public function __tostring() {
-		return 'SessionUser';
-	}
+	public $anonymous = true; // will only be true for very first layer
 
 	public function hasAccess() {
 		return (bool)rand(0, 1);
+	}
+
+	public function logout() {
+		// Do what exactly? Invalidate session layer how? Have a standard (ROW) implementation or let APP handle fully?
+		// Change _SESSION here or in auth\Session?
+	}
+
+	public function displayName() {
+		return 'Anonymous';
+	}
+
+	public function __tostring() {
+		return 'SessionUser: ' . $this->displayName();
 	}
 
 }
