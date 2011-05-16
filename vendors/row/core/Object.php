e5da02336304dbe14024f0b9a497aa5df904a6e0
Rudie Dirkx (ezCompany, HP) <github@hotblocks.nl>
2011-05-16 15:43:57 +0200

    Tiny call_user_func performance

--- vendors/row/core/Object.php
+++ vendors/row/core/Object.php
@@ -33,26 +33,6 @@ abstract class Object {
 
 	protected function _init() {}
 
-	/* this is not a good idea - `is_callable` will be completely useless *
-	static public $_methods = array();
-
-	static public function extend( $name, $function ) {
-		$class = strtolower(get_called_class());
-		$name = strtolower($name);
-		self::$_methods[$class][$name] = $function;
-	}
-
-	public function __call( $name, $args ) {
-		$name = strtolower($name);
-		$class = strtolower(get_class($this));
-		array_unshift($args, $this);
-		if ( isset(self::$_methods[$class][$name]) ) {
-			return call_user_func_array(self::$_methods[$class][$name], $args);
-		}
-	}
-	/**/
-
-
 	public function _exists( $k ) {
 		return property_exists($this, $k);
 	}
@@ -64,15 +44,12 @@ abstract class Object {
 	public function _fire( $fn, $args = array() ) {
 		$fn = '_'.$fn;
 		if ( method_exists($this, $fn) ) {
-			if ( 0 == count($args) ) {
+			if ( !isset($args[0]) ) {
 				return $this->$fn();
 			}
-/*			else if ( 1 == count($args) ) {
+			else if ( !isset($args[1]) ) {
 				return $this->$fn($args[0]);
 			}
-			else if ( 2 == count($args) ) {
-				return $this->$fn($args[0], $args[1]);
-			}*/
 			else {
 				return call_user_func_array(array($this, $fn), $args);
 			}
