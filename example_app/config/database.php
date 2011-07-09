6837e88f23e3ed92a492097da84c4846c2745b27
Rudie Dirkx <github@hotblocks.nl>
2011-07-09 17:14:57 +0200

    more env setup

--- example_app/config/database.php
+++ example_app/config/database.php
@@ -7,8 +7,8 @@ require(ROW_VENDOR_ROW_PATH.'/database/adapter/MySQLi.php');
 use row\database\adapter;
 use row\database\Model;
 
-//$db = adapter\MySQL::open(array('user' => 'blog', 'dbname' => 'blog', 'names' => 'utf8')); // typeof MySQLi (probably)
-$db = adapter\SQLite::open(array('path' => ROW_APP_PATH.'/runtime/blog.sqlite3'));
+//$db = adapter\MySQL::open(array('user' => DB_HOST, 'dbname' => DB_NAME, 'names' => 'utf8')); // typeof MySQLi (probably)
+$db = adapter\SQLite::open(array('path' => DB_PATH));
 
 if ( !$db->connected() ) {
 	exit('I could not connect to the specified database. Edit <u>app/config/database.php</u> and import <u>app/_project/blog.sql</u> to fix this problem!');
--- initial_empty_start_app/config/database.php
+++ initial_empty_start_app/config/database.php
@@ -7,8 +7,7 @@ require(ROW_VENDOR_ROW_PATH.'/database/adapter/MySQLi.php');
 use row\database\adapter;
 use row\database\Model;
 
-//$db = adapter\MySQL::open(array('user' => 'username', 'pass' => 'password', 'dbname' => 'database', 'names' => 'utf8'));
-$db = adapter\SQLite::open(array('path' => ROW_APP_PATH.'/runtime/blog.sqlite3'));
+$db = adapter\MySQL::open(array('user' => DB_HOST, 'dbname' => DB_NAME, 'names' => 'utf8'));
 
 if ( !$db->connected() ) {
 	exit('I could not connect to the specified database. Edit <u>app/config/database.php</u> and import <u>app/_project/blog.sql</u> to fix this problem!');
