9bf2e37d13571763549ac6d15dcd8b05e39c2ca9
Rudie Dirkx <github@hotblocks.nl>
2011-08-05 18:33:09 +0200

    pagoda stuff

--- example_app/config/base.php
+++ example_app/config/base.php
@@ -6,6 +6,8 @@ define( 'ROW_PATH', dirname(ROW_APP_PATH) ); // Absolute root
 define( 'ROW_VENDORS_PATH', ROW_PATH.'/vendors' ); // Folders with all the vendor folders
 define( 'ROW_VENDOR_ROW_PATH', ROW_VENDORS_PATH.'/row' ); // The folder for the framework vendor Rudie On Wheels
 
+print_r($_SERVER);
+
 if ( isset($_SERVER['ENVIRONMENT']) && $_SERVER['ENVIRONMENT'] == 'PAGODA' ) {
 	require(__DIR__.'/pagoda.php');
 }
