e676bac7c3393da0bae63043703aa635582cd2f6
Rudie Dirkx <github@hotblocks.nl>
2011-10-27 18:27:56 +0200

    default env in initial start app

--- initial_empty_start_app/config/default.env.php
+++ initial_empty_start_app/config/default.env.php
@@ -2,9 +2,13 @@
 
 define('APPLICATION_ENV', 'dev');
 
+# mysql, pgsql: server with auth
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'app');
+
+# sqlite: local file
+define('DB_PATH', ROW_APP_PATH.'/runtime/database.sqlite3');
 
 
